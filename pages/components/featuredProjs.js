a355891fcc3eb76d442ddfd9e6a5afb1d3dc792f
TroyAWilson <wilsontr@sonoma.edu>
2023-03-11 12:03:37 -0800

    remove arrows

--- pages/components/featuredProjs.js
+++ pages/components/featuredProjs.js
@@ -31,18 +31,18 @@ const FeaturedProjs = () => {
             link:'/projects/pokeinfo',
             gif:['./pokeinfogif-desktop.gif','./pokeinfogif-tablet.gif','./pokeinfogif-mobile.gif']
         },
-        {
-            title:"NASA's Neurodiversity Network (N3)",
-            description:"NASA's Neurodiversity Network (N3) is providing a pathway to NASA participation and STEM employment for neurodiverse learners, with a focus on those on the autism spectrum.",
-            link:'/projects/n3',
-            gif:['./n3gif-desktop.gif','n3gif-tablet.gif','n3gif-mobile.gif']
-        },
-        {
-            title:"Eclipse Megamovie 2024",
-            description:"This first-of-its-kind citizen science project resulted in a collection of tens of thousands of photos submitted by hundreds of volunteers from locations across the United States during and after the August 21, 2017, total solar eclipse.",
-            link:'/projects/eclipse',
-            gif:['eclipsegif-desktop.gif','eclipsegif-tablet.gif','eclipsegif-mobile.gif']
-        },
+        // {
+        //     title:"NASA's Neurodiversity Network (N3)",
+        //     description:"NASA's Neurodiversity Network (N3) is providing a pathway to NASA participation and STEM employment for neurodiverse learners, with a focus on those on the autism spectrum.",
+        //     link:'/projects/n3',
+        //     gif:['./n3gif-desktop.gif','n3gif-tablet.gif','n3gif-mobile.gif']
+        // },
+        // {
+        //     title:"Eclipse Megamovie 2024",
+        //     description:"This first-of-its-kind citizen science project resulted in a collection of tens of thousands of photos submitted by hundreds of volunteers from locations across the United States during and after the August 21, 2017, total solar eclipse.",
+        //     link:'/projects/eclipse',
+        //     gif:['eclipsegif-desktop.gif','eclipsegif-tablet.gif','eclipsegif-mobile.gif']
+        // },
     ];
 
     return(
@@ -50,8 +50,8 @@ const FeaturedProjs = () => {
             <div style={{border:'15px solid #F8CE6B', position:'relative'}}>
                 <div style={{display:'flex', justifyContent:'center', alignItems:'center', position:'absolute', backgroundColor:'rgba(0,0,0,0.8)', height:'100%', width:'100%', color:'white'}}>
                     
-                    <ArrowBackIosIcon className='featuredButtons' onClick={()=>changeProject(-1)} style={{left:0}}/>
-                    <ArrowForwardIosIcon className='featuredButtons' onClick={()=>changeProject(1)} style={{right:0}}/>
+                    {/* <ArrowBackIosIcon className='featuredButtons' onClick={()=>changeProject(-1)} style={{left:0}}/>
+                    <ArrowForwardIosIcon className='featuredButtons' onClick={()=>changeProject(1)} style={{right:0}}/> */}
 
                     <div className='featuredContent'>
                         
